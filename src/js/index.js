47b3364d7a7c50338d1940d6866cd5ff809a9305
Krisztián Kecskés <kecsk.krisztian@gmail.com>
2018-11-24 14:09:18 +0100

    master | Add axios and profile picture from git to the header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
+import "@babel/polyfill";
+import axios from 'axios';
 
+const getAvatar = async function() {
+
+  try {
+    const { data: { avatar_url } } = await axios.get('https://api.github.com/users/krisztiankecskes');
+    if(!avatar_url) return false;
+
+    const avatar = document.querySelector('.header__profile-picture');
+    if(!avatar) return false;
+
+    avatar.style.backgroundImage = `url(${avatar_url})`;
+    avatar.style.backgroundSize = 'cover';
+
+  } catch(e) {
+    console.log('Getting avatar url is failed!');
+  }
+
+};
+
+(function () {
+  getAvatar();
+})();
